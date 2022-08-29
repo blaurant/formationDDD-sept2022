f6c47307b3064e983ac13fe67da65cff33b4a98d
bernard <bernard@laurant.fr>
2022-08-29 17:34:53 +0200

    Table lifecycle test not working

--- src/test/java/com/restaurant/domain/TableTest.java
+++ src/test/java/com/restaurant/domain/TableTest.java
@@ -24,12 +24,37 @@ public class TableTest {
         Table table = Table.of(1, 2);
         assertThat(table.number()).isEqualTo(1);
         assertThat(table.capacity()).isEqualTo(2);
+        assertThat(table.state()).isEquals(Table.SET);
     }
 
     @Test
     public void identityTest() {
-        Assertions.assertThat(Table.of(2, 2)).isEqualTo(Table.of(2, 2));
-        Assertions.assertThat(Table.of(2, 2)).isNotEqualTo(Table.of(3, 2));
+        assertThat(Table.of(2, 2)).isEqualTo(Table.of(2, 2));
+        assertThat(Table.of(2, 2)).isNotEqualTo(Table.of(3, 2));
+    }
+
+    @Test
+    public void assignTest() {
+        assertThat(Table.of(1,2)
+                .assign()
+                .state()).isEquals(Table.ACCUPIED);
+    }
+
+    @Test
+    public void clearTest() {
+        assertThat(Table.of(1,2)
+                .assign()
+                .clear()
+                .state()).isEquals(Table.FREED);
+    }
+
+    @Test
+    public void setTest() {
+        assertThat(Table.of(1,2)
+                .assign()
+                .clear()
+                .set()
+                .state()).isEquals(Table.SET);
     }
 }
 
