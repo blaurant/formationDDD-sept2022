a3c29eb72c73206354776b802a7962cdfdd403da
bernard <bernard@laurant.fr>
2022-08-29 16:16:12 +0200

    Table test not working

--- src/test/java/com/restaurant/domain/TableTest.java
+++ src/test/java/com/restaurant/domain/TableTest.java
@@ -1,7 +1,29 @@
 package com.restaurant.domain;
 
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
+import sun.tools.jconsole.Tab;
 
+import static DDD.framework.Integers.requireBetween;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+/**
+ * First we test identity, then invariants, than life cycle.
+ *
+ */
 public class TableTest {
+
+    @Test
+    public void buildTest() {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> new Table(null));
+    }
+
+    @Test
+    public void identityTest() {
+        Assertions.assertThat(new Table(2)).isEqualTo(new Table(2));
+        Assertions.assertThat(new Table(2)).isNotEqualTo(new Table(3));
+    }
 
 }
